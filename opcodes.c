0daf2ae20c09941e8725aa3dac7a4545e6379256
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-14 21:07:01 +0100

    Fix: Still wrong for checker

--- opcodes.c
+++ opcodes.c
@@ -70,7 +70,7 @@ void pint(stack_t **stack, char *token, unsigned int line_number)
 
 	if (current == NULL)
 	{
-		fprintf(stderr, "L<%d>: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", current->n);
